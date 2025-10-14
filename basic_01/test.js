dd6b1f629a22f37515c9758550021b9b65573240
ahmed-7384 <ahamedsk76@gmail.com>
2025-10-14 05:45:51 +0000

    save for today

--- basic_01/test.js
+++ basic_01/test.js
@@ -1,13 +1,13 @@
-const score =400
-console.log(typeof score);
+// const score =400
+// console.log(typeof score);
 
-const balance = new Number(100);
+// const balance = new Number(100);
 
-console.log(balance)
+// console.log(balance)
 
-console.log(balance.toString());
+// console.log(balance.toString());
 
-console.log(typeof balance);
+// console.log(typeof balance);
 
 
 
@@ -17,5 +17,39 @@ console.log(typeof balance);
 
 // console.log(typeof newBalance);
 
+//****************Array***************//
 
+ const myArray = [1, 2, 5, 6, 8];
 
+// console.log(myArray);
+// const newArray2 = new Array(5, 7, 9, 4, 1);
+
+// console.log(newArray2);
+//  console.log(newArray2[4]);
+
+//  myArray.push(7)    //to add in array//
+
+//  console.log(myArray);
+// myArray.pop();           //to remove a in array//
+
+// const newArr = myArray.join();
+
+// console.log(myArray);
+
+// console.log(typeof newArr);
+
+/***********slice and splice ******************/
+
+// console.log("A", myArray);
+//  const myn1 = myArray.slice(1, 3);
+
+//  console.log(myn1);
+//  console.log("B", myArray);
+ 
+  const myn2 = myArray.splice(2, 2);
+
+ console.log(myn2);
+ console.log("c", myArray);
+ 
+ console.log(myArray);
+ 
