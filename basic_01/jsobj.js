ce7bc8774a20e0316c298f5e50a153fb05068250
ahmed-7384 <ahamedsk76@gmail.com>
2025-10-14 13:15:47 +0000

    saved

---
+++ basic_01/jsobj.js
@@ -0,0 +1,13 @@
+//object literials//
+
+const jsUser = {
+    name: "Ahamed",
+    age: 28,
+    email: "ahamedsk@gmail.com",
+    location: "tamluk",
+    isloggein: "false",
+    lastloggedin: ["monday", "sunday"]
+
+}
+console.log(jsUser.email);
+console.log(jsUser["location"]);
