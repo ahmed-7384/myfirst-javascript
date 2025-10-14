02315ded7bf99cfe6e49f4957993d136a8c3a1c0
ahmed-7384 <ahamedsk76@gmail.com>
2025-10-14 08:57:38 +0000

    saved

---
+++ basic_01/array.js
@@ -0,0 +1,32 @@
+// const marvel_heros = ["Iron Man", "Captan America", "Spider Man"];
+
+// const dc_heros = ["BatMan", "Super Man","Aqua Man"];
+
+// // console.log(marvel_heros);
+// // console.log(dc_heros);
+
+// // marvel_heros.push(dc_heros);
+// // console.log (marvel_heros)
+
+// // const avengers = marvel_heros.concat(dc_heros)
+// // console.log(avengers);
+
+// const avengersAssamble = [...marvel_heros, ...dc_heros];
+// console.log(avengersAssamble);
+
+
+// const anotherArray = [1, 2, 3, [4, 5, 6, [7, 8, 9]]];
+// //  console.log(anotherArray);
+ 
+//  const realArray =anotherArray.flat(Infinity);
+
+//  console.log(realArray);
+ 
+const myName = Array.from("sahensa");      //to convert any input to a Array//
+// console.log(myName);
+
+let score1 = 400;
+let score2 = 50;
+let score3 = 600;
+
+console.log((Array.of(score1, score2, score3)));
