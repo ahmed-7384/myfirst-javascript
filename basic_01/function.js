3ba52be0e9ee366fc9e9f48cf0784f9f3383fabb
ahmed-7384 <ahamedsk76@gmail.com>
2025-10-16 13:10:51 +0000

    saved

---
+++ basic_01/function.js
@@ -0,0 +1,72 @@
+// console.log("A");
+// console.log("H");
+// console.log("A");
+// console.log("M");
+// console.log("E");
+// console.log("D");
+
+// function myName(){
+//     console.log("A");
+// console.log("H");
+// console.log("A");
+// console.log("M");
+// console.log("E");
+// console.log("D");
+
+// }
+
+// myName()
+
+// function addTwoNumber(number1, number2){
+
+// // let result = number1 + number2
+// return number1 + number2
+
+
+// }
+// const result = addTwoNumber(5, 6)
+
+// // console.log("result", result);
+
+// function justLoggedIn(username ="sahen"){
+//     if(username === undefined){
+
+//         console.log("please enter your name");
+// return
+//     }
+//     // return `${username} just logged in`
+// }
+// // console.log(justLoggedIn("ahamed"));
+// console.log(justLoggedIn());
+
+
+
+// function calculateCart(val1, val2, ...number1) {
+//     return number1
+// }
+// console.log(calculateCart(2, 200, 300, 600, 500));
+ 
+// const user = {
+//     username: "Ahamed",
+//     prices: 199
+// }
+
+function handleObject(anyobject){
+    console.log(`username is ${anyobject.username} and prices is ${anyobject.prices}`);
+    
+}
+
+// handleObject(user)
+handleObject (
+{
+    username: "sahensa",
+    prices: 399
+})
+
+const newArrayy = [200, 300, 400, 500]
+
+function myNewArray(newarray) {
+    return newarray[2]
+    
+}
+console.log(myNewArray(newArrayy));
