3e502b0b307b9a0dfe3515434993a47bfda5f227
ahmed-7384 <ahamedsk76@gmail.com>
2025-10-17 06:20:08 +0000

    save

---
+++ basic_01/scope.js
@@ -0,0 +1,38 @@
+function one(){
+
+    const mobile = "motorola"
+
+    function two(){
+
+        const laptop = "hp"
+
+ function three(){
+
+    const bike = "ktm"
+    function four(){
+        const scoter = "alpha"
+
+        console.log(mobile);
+        // console.log(laptop);
+        console.log(bike);
+        console.log(scoter);
+    
+        
+    }
+
+    four()
+
+ } 
+
+ three()
+ console.log(laptop);
+ 
+
+    }
+
+two()
+
+
+}
+
+one()
